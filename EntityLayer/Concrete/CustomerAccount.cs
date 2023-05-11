7489a871a10b76c07b815d983c7bdb1899e8894e
Azamat <73704375+azamat1696@users.noreply.github.com>
2023-05-11 11:52:33 +0300

    Add project files.

---
+++ EntityLayer/Concrete/CustomerAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class CustomerAccount
+    {
+        [Key]
+        public int CustomerAccoubtID { get; set; }
+
+        public string CustomerAccountNumber { get; set; }
+
+        public string CusromerAccountCurrency { get; set;}
+
+        public decimal CusromerAccountBalance { get; set; }
+
+        public string BankBranch { get; set; }
+
+
+
+    }
+}
